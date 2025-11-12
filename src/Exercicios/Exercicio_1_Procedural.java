667414ce1a7239b2beeb1d81c21df4144dc4e6cb
FernandaLucas-G <fernandalucas.contato@gmail.com>
2025-11-12 01:43:47 -0300

    Update Exercicio_1_Procedural.java

--- src/Exercicios/Exercicio_1_Procedural.java
+++ src/Exercicios/Exercicio_1_Procedural.java
@@ -1,18 +1,18 @@
 package Exercicios;
 import java.util.Scanner;
 
-public class Exercicio_1_Procedural { // Procedural é como escrevemos no Python, por exemplo
+public class Exercicio_1_Procedural { // Procedural: código organizado em passos sequenciais, como em Python
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("Digite a primeira nota: ");
-		double n1 = teclado.nextDouble(); // Ele vai ler o proximo valor digitado e guardar em N1
+		double n1 = teclado.nextDouble(); // Captura a nota do aluno para calcular a média posteriormente
 		System.out.print("Digite a segunda nota: ");
 		double n2 = teclado.nextDouble();
 		System.out.print("Digite a terceria nota: ");
 		double n3 = teclado.nextDouble();
 		
-		// Double guarda números com casas decimais, ou seja, números com vírgula
+		// double armazena números com casas decimais
 		
 		double media = (n1 + n2 + n3) / 3;
 		System.out.println("Média = " + media);
